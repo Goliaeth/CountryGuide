2944bbed6dcf4b0bc043d2fbd75f1667dacb8467
Goliaeth <75118259+Goliaeth@users.noreply.github.com>
2022-09-10 17:54:03 +0500

    add retrofit

---
+++ app/src/main/java/com/example/countryguide/RemoteDataSource.kt
@@ -0,0 +1,19 @@
+package com.example.countryguide
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface RestCountriesService {
+    @GET("name/{name}")
+    fun getCountryByName(@Path("name") countryName: String): List<Country>
+}
+
+var retrofit = Retrofit.Builder()
+    .baseUrl("https://countriesinfo21.herokuapp.com/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+var restCountriesService = retrofit.create(RestCountriesService::class.java)
